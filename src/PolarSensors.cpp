c576e38a6a53a2b4d4edd1afb1081134f948b60a
Jasmin Dschessica <juicy@jessica.web>
2020-09-30 22:17:36 +0200

    started work on launch file params.

--- src/PolarSensors.cpp
+++ src/PolarSensors.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "PolarSensors.h"
+#include "VelodyneVLP"
 
 PolarSensors::PolarSensors(const float dimX, const float dimY, const float dimZ, const float cellSize)
     : _nh(), _prvnh("~"), _dimX(dimX), _dimY(dimY), _dimZ(dimZ), _cellSize(cellSize), /**_listener(std::make_unique<tf::TransformListener>()),**/ _cellsX(0),
@@ -14,6 +15,7 @@ PolarSensors::PolarSensors(const float dimX, const float dimY, const float dimZ,
   // launchfile parameters, default vals for VLP16
   _prvnh.param<std::string>("sensorType", _sensorType, "VLP16");
   _prvnh.param<std::string>("laserDataTopic", _laserDataTopic, "puck_rear/velodyne_points");
+  _prvnh.param<int>("raysIncl", _raysIncl, 16);
   _prvnh.param<double>("inclMin", _inclMin, -0.26180); //-15°
   _prvnh.param<double>("inclMax", _inclMax, 0.26180);  //+15°
   _prvnh.param<double>("inclRes", _inclRes, 0.034907); // 2°
@@ -21,8 +23,77 @@ PolarSensors::PolarSensors(const float dimX, const float dimY, const float dimZ,
   _prvnh.param<double>("azimMax", _azimMax, 6.28319);    // 360°
   _prvnh.param<double>("azimRes", _azimRes, 0.00349066); // 0.2°
 
+  std::cout << __PRETTY_FUNCTION__ << "launch check" << std::endl;
+  std::cout << "raysIncl = " << _raysIncl << std::endl;
+  std::cout << "inclMin = " << _inclMin << std::endl;
+  std::cout << "inclMax = " << _inclMax << std::endl;
+  std::cout << "inclRes = " << _inclRes << std::endl;
+  std::cout << "azimMin = " << _azimMin << std::endl;
+  std::cout << "azimMax = " << _azimMax << std::endl;
+  std::cout << "azimRes = " << _azimRes << std::endl;
+
   _subPointcloud = _nh.subscribe(_laserDataTopic, 1, &PolarSensors::callbackPointcloud, this);
 }
 virtual PolarSensors::~PolarSensors() {}
 
-void PolarSensors::callbackPointcloud(const pcl::PointCloud<pcl::PointXYZ>& cloud) {}
+void PolarSensors::init(const pcl::PointCloud<pcl::PointXYZ>& cloud)
+{
+  if(_sensor || _space)
+    return;
+
+  // 1. INITIALIZE SPACE
+  unsigned int cellsX = static_cast<unsigned int>(std::round(_dimX / _cellSize));
+  unsigned int cellsY = static_cast<unsigned int>(std::round(_dimY / _cellSize));
+  unsigned int cellsZ = static_cast<unsigned int>(std::round(_dimZ / _cellSize));
+
+  const unsigned int xFac = cellsX / 64;
+  const unsigned int yFac = cellsY / 64;
+  const unsigned int zFac = cellsZ / 64;
+
+  _cellsX = xFac * 64;
+  _cellsY = yFac * 64;
+  _cellsZ = zFac * 64;
+
+  _space = std::make_unique<obvious::TsdSpace>(_cellSize, obvious::LAYOUT_64x64x64, _cellsX, _cellsY, _cellsZ);
+  _space->setMaxTruncation(3.0 * _cellSize);
+  // initial sensor pose: middle of space
+  double tr[3];
+  _space->getCentroid(tr);
+  double          tf[16] = {1, 0, 0, tr[0], 0, 1, 0, tr[1], 0, 0, 1, tr[2], 0, 0, 0, 1};
+  obvious::Matrix Tinit(4, 4);
+  Tinit.setIdentity();
+  Tinit.setData(tf);
+
+  // 2. INITIALIZE SENSOR
+  if(_sensorType == 'VLP16')
+  {
+    _firingSeq = {0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15}; // todo: do this via launchfile & pushback into vec here. better: yaml file
+    _sensor    = std::make_unique<obvious::SensorPolar>(_raysIncl, _inclMin, _inclMax, _inclRes, _azimMin, _azimMax, _azimRes, _firingSeq);
+  }
+  else if(_sensorType == 'HDL32E')
+  {
+    _firingSeq = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31};
+    _sensor    = std::make_unique<obvious::SensorPolar>(_raysIncl, _inclMin, _inclMax, _inclRes, _azimMin, _azimMax, _azimRes, _firingSeq);
+  }
+  else if((_sensorType == 'OUSTEROS0') || (_sensorType == 'TILT3D') || (_sensorType == 'TILT4D'))
+  {
+    _sensor = std::make_unique<obvious::SensorPolar>(_raysIncl, _inclMin, _inclMax, _inclRes, _azimMin, _azimMax, _azimRes);
+  }
+  else
+  {
+    std::cout << __PRETTY_FUNCTION__
+              << "invalid sensorType. Please check launchfile. Wanna add a new sensor? pls add it to polarsensors.launch and initialize it here."
+              << std::endl;
+  }
+
+  _sensor->setTransformation(Tinit);
+  std::cout << "Sensor pose after transforming to the middle of TSD space: " << std::endl;
+  _sensor->getTransformation().print();
+}
+
+void PolarSensors::callbackPointcloud(const pcl::PointCloud<pcl::PointXYZ>& cloud)
+{
+  // if(!_sensor || !_space) // if neither sensor nor space have been initialized yet -> do so in init routine
+  if(!_space) // if space hasn't been initialized yet -> do so in init routine
+    this->init(cloud);
+}
--- src/PolarSensors.h
+++ src/PolarSensors.h
@@ -5,8 +5,8 @@
  *      Author: Jasmin Ziegler
  */
 
+#include "obvision/reconstruct/space/SensorPolar.h"
 #include "obvision/reconstruct/space/TsdSpace.h"
-#include "obvision/reconstruct/space/VelodyneVLP16.h"
 #include <pcl_ros/point_cloud.h>
 #include <ros/ros.h>
 #include <string>
@@ -23,24 +23,23 @@ public:
 private:
   void init(const pcl::PointCloud<pcl::PointXYZ>& cloud);
 
-  std::unique_ptr<obvious::TsdSpace> _space;
-  //   std::unique_ptr<obvious::VelodyneVLP16> _sensor; // nicht als member machen sondern übergeben in den Methoden, damit ich über launchfile sensortype
-  //   bestimmen kann
+  std::unique_ptr<obvious::TsdSpace>    _space;
+  std::unique_ptr<obvious::SensorPolar> _sensor;
 
   ros::Subscriber _subPointcloud;
 
   ros::NodeHandle _nh;
   ros::NodeHandle _prvnh;
 
-  std::string _sensorType;
-  std::string _laserDataTopic;
-  double      _inclMin;
-  double      _inclMax;
-  double      _inclRes;
-  double      _azimMin;
-  double      _azimMax;
-  double      _azimRes;
-
+  int              _raysIncl;
+  std::string      _sensorType;
+  std::string      _laserDataTopic;
+  double           _inclMin;
+  double           _inclMax;
+  double           _inclRes;
+  double           _azimMin;
+  double           _azimMax;
+  double           _azimRes;
   std::vector<int> _firingSeq;
 
   float _dimX;
