92731f378a35f1fda4241ce9695755df63f4f3a4
Jasmin Dschessica <juicy@jessica.web>
2020-10-02 19:34:34 +0200

    finished ROS node for instantiating SensorPolar which works as ohm_tsd_slam sensor model for: Velodyne VLP16, Velodyne HDL32E, OUSTEROS0, tilt3d with SICK TIM 571, tilt4d with VLP16. Supports axis aligned raycast, redblue render space, sensor raycast. launch file included.

--- src/PolarSensors.cpp
+++ src/PolarSensors.cpp
@@ -6,7 +6,8 @@
  */
 
 #include "PolarSensors.h"
-#include "VelodyneVLP"
+#include "obvision/reconstruct/space/RayCast3D.h"
+#include "obvision/reconstruct/space/RayCastAxisAligned3D.h"
 
 PolarSensors::PolarSensors(const float dimX, const float dimY, const float dimZ, const float cellSize)
     : _nh(), _prvnh("~"), _dimX(dimX), _dimY(dimY), _dimZ(dimZ), _cellSize(cellSize), /**_listener(std::make_unique<tf::TransformListener>()),**/ _cellsX(0),
@@ -23,18 +24,25 @@ PolarSensors::PolarSensors(const float dimX, const float dimY, const float dimZ,
   _prvnh.param<double>("azimMax", _azimMax, 6.28319);    // 360°
   _prvnh.param<double>("azimRes", _azimRes, 0.00349066); // 0.2°
 
-  std::cout << __PRETTY_FUNCTION__ << "launch check" << std::endl;
+  std::cout << __PRETTY_FUNCTION__ << " LAUNCH CHECK " << std::endl;
+  std::cout << "sensorType = " << _sensorType << std::endl;
   std::cout << "raysIncl = " << _raysIncl << std::endl;
-  std::cout << "inclMin = " << _inclMin << std::endl;
-  std::cout << "inclMax = " << _inclMax << std::endl;
-  std::cout << "inclRes = " << _inclRes << std::endl;
-  std::cout << "azimMin = " << _azimMin << std::endl;
-  std::cout << "azimMax = " << _azimMax << std::endl;
-  std::cout << "azimRes = " << _azimRes << std::endl;
+  std::cout << "inclMin = " << _inclMin << " = " << RAD2DEG(_inclMin) << "°" << std::endl;
+  std::cout << "inclMax = " << _inclMax << " = " << RAD2DEG(_inclMax) << "°" << std::endl;
+  std::cout << "inclRes = " << _inclRes << " = " << RAD2DEG(_inclRes) << "°" << std::endl;
+  std::cout << "azimMin = " << _azimMin << " = " << RAD2DEG(_azimMin) << "°" << std::endl;
+  std::cout << "azimMax = " << _azimMax << " = " << RAD2DEG(_azimMax) << "°" << std::endl;
+  std::cout << "azimRes = " << _azimRes << " = " << RAD2DEG(_azimRes) << "°" << std::endl;
 
-  _subPointcloud = _nh.subscribe(_laserDataTopic, 1, &PolarSensors::callbackPointcloud, this);
+  _subPointcloud       = _nh.subscribe(_laserDataTopic, 1, &PolarSensors::callbackPointcloud, this);
+  _pubAxisAlignedCloud = _nh.advertise<pcl::PointCloud<pcl::PointXYZRGBNormal> >("axisAlignedCloud", 1);
+  _tfActive            = false;
+  _virginPush          = false;
+  _tfMyFrameID         = "map";
+  _tfCallbackFrameID   = "ground_truth"; // for gazebo bags, change this
 }
-virtual PolarSensors::~PolarSensors() {}
+
+PolarSensors::~PolarSensors() {}
 
 void PolarSensors::init(const pcl::PointCloud<pcl::PointXYZ>& cloud)
 {
@@ -49,10 +57,9 @@ void PolarSensors::init(const pcl::PointCloud<pcl::PointXYZ>& cloud)
   const unsigned int xFac = cellsX / 64;
   const unsigned int yFac = cellsY / 64;
   const unsigned int zFac = cellsZ / 64;
-
-  _cellsX = xFac * 64;
-  _cellsY = yFac * 64;
-  _cellsZ = zFac * 64;
+  _cellsX                 = xFac * 64;
+  _cellsY                 = yFac * 64;
+  _cellsZ                 = zFac * 64;
 
   _space = std::make_unique<obvious::TsdSpace>(_cellSize, obvious::LAYOUT_64x64x64, _cellsX, _cellsY, _cellsZ);
   _space->setMaxTruncation(3.0 * _cellSize);
@@ -65,24 +72,24 @@ void PolarSensors::init(const pcl::PointCloud<pcl::PointXYZ>& cloud)
   Tinit.setData(tf);
 
   // 2. INITIALIZE SENSOR
-  if(_sensorType == 'VLP16')
+  if(_sensorType == "VLP16")
   {
-    _firingSeq = {0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15}; // todo: do this via launchfile & pushback into vec here. better: yaml file
+    _firingSeq = {0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15};
     _sensor    = std::make_unique<obvious::SensorPolar>(_raysIncl, _inclMin, _inclMax, _inclRes, _azimMin, _azimMax, _azimRes, _firingSeq);
   }
-  else if(_sensorType == 'HDL32E')
+  else if(_sensorType == "HDL32E")
   {
     _firingSeq = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31};
     _sensor    = std::make_unique<obvious::SensorPolar>(_raysIncl, _inclMin, _inclMax, _inclRes, _azimMin, _azimMax, _azimRes, _firingSeq);
   }
-  else if((_sensorType == 'OUSTEROS0') || (_sensorType == 'TILT3D') || (_sensorType == 'TILT4D'))
+  else if((_sensorType == "OUSTEROS0") || (_sensorType == "TILT3D") || (_sensorType == "TILT4D"))
   {
     _sensor = std::make_unique<obvious::SensorPolar>(_raysIncl, _inclMin, _inclMax, _inclRes, _azimMin, _azimMax, _azimRes);
   }
   else
   {
-    std::cout << __PRETTY_FUNCTION__
-              << "invalid sensorType. Please check launchfile. Wanna add a new sensor? pls add it to polarsensors.launch and initialize it here."
+    std::cout << __PRETTY_FUNCTION__ << "invalid sensorType. Please check launchfile. Wanna add a new sensor? pls add it to polarsensors.launch and "
+                                        "initialize it here. More detailed info on how to add a new sensor in polarsensors.launch"
               << std::endl;
   }
 
@@ -91,9 +98,287 @@ void PolarSensors::init(const pcl::PointCloud<pcl::PointXYZ>& cloud)
   _sensor->getTransformation().print();
 }
 
+void PolarSensors::sortPointcloudTilt4d(const pcl::PointCloud<pcl::PointXYZ>* cloud, pcl::PointCloud<pcl::PointXYZ>* sortedCloud)
+{
+  // sort incoming incl rays in vector of vectors (buckets) - one bucket for each inclination angle
+  // store new pointcloud in sortedCloud so it can be accessed from callbackPointcloud for calculating depth values and setting vector depthData
+}
+
+void PolarSensors::pubAxisAlignedRaycaster(void)
+{
+  static obvious::obfloat*      coords  = new obvious::obfloat[_cellsX * _cellsY * _cellsZ * 3];
+  static obvious::obfloat*      normals = new obvious::obfloat[_cellsX * _cellsY * _cellsZ * 3];
+  static unsigned char*         rgb     = new unsigned char[_cellsX * _cellsY * _cellsZ * 3];
+  static unsigned int           seq     = 0;
+  obvious::RayCastAxisAligned3D raycasterAxisAligned;
+  unsigned int                  cnt = 0;
+
+  raycasterAxisAligned.calcCoords(_space.get(), coords, normals, rgb, &cnt);
+
+  pcl::PointCloud<pcl::PointXYZRGBNormal> cloud;
+  obvious::obfloat                        tr[3];
+  _space->getCentroid(tr);
+
+  for(unsigned int i = 0; i < cnt; i += 3)
+  {
+    pcl::PointXYZRGBNormal p;
+    p.x = coords[i] - tr[0];
+    p.y = coords[i + 1] - tr[1];
+    p.z = coords[i + 2] - tr[2];
+
+    p.normal_x = normals[i] - tr[0];
+    p.normal_y = normals[i + 1] - tr[1];
+    p.normal_z = normals[i + 2] - tr[2];
+
+    p.r = rgb[i];
+    p.g = rgb[i + 1];
+    p.b = rgb[i + 2];
+
+    cloud.push_back(p);
+  }
+
+  cloud.header.frame_id = _tfMyFrameID;
+  cloud.header.seq      = seq++;
+  std::cout << __PRETTY_FUNCTION__ << " publishing " << cloud.size() << " points" << std::endl;
+  _pubAxisAlignedCloud.publish(cloud);
+}
+
+void PolarSensors::redBlueRenderSpace(pcl::PointCloud<pcl::PointXYZRGB>& cloud)
+{
+  static unsigned int seq = 0;
+  struct Color
+  {
+    Color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
+    Color() : r(0), g(0), b(0) {}
+    void red(uint8_t val) { r = val; }
+    void blue(uint8_t val) { b = val; }
+    uint8_t           r;
+    uint8_t           g;
+    uint8_t           b;
+  };
+
+  obvious::Matrix*   cellCoordsHom = obvious::TsdSpacePartition::getCellCoordsHom();
+  obvious::Matrix*   partCoords    = obvious::TsdSpacePartition::getPartitionCoords();
+  unsigned int       partSize      = (_space->getPartitions())[0][0][0]->getSize();
+  stdVecEig3d        centers;
+  std::vector<Color> colors;
+  obvious::obfloat   tr[3];
+  _space->getCentroid(tr);
+  for(unsigned int pz = 0; pz < _space->getPartitionsInZ(); pz++)
+  {
+    for(unsigned int py = 0; py < _space->getPartitionsInY(); py++)
+    {
+      for(unsigned int px = 0; px < _space->getPartitionsInX(); px++)
+      {
+        obvious::TsdSpacePartition* part = _space->getPartitions()[pz][py][px];
+        if(part->isInitialized() && !part->isEmpty())
+        {
+          obvious::obfloat t[3];
+          part->getCellCoordsOffset(t);
+          for(unsigned int c = 0; c < partSize; c++)
+          {
+            Eigen::Vector3d center;
+            center(0) = (*cellCoordsHom)(c, 0) + t[0];
+            center(1) = (*cellCoordsHom)(c, 1) + t[1];
+            center(2) = (*cellCoordsHom)(c, 2) + t[2];
+            // if(center(1) > _space->getMaxY() / 2.0)
+            //   continue;
+            obvious::obfloat tsd = part->getTsd((*partCoords)(c, 0), (*partCoords)(c, 1), (*partCoords)(c, 2));
+            if((isnan(tsd)) || (tsd > std::abs(1.1)))
+              continue;
+
+            Color tsdColor; //(0, 0, 0);
+            if(tsd < 0.0)   // red
+              tsdColor.red(static_cast<unsigned char>(-1.0 * tsd * 255.0));
+            else
+              tsdColor.blue(static_cast<unsigned char>(tsd * 255.0));
+
+            centers.push_back(center);
+            colors.push_back(tsdColor);
+          }
+        }
+      }
+    }
+  }
+  if((centers.size() == colors.size()) && (centers.size() != 0))
+  {
+    for(unsigned int i = 0; i < centers.size(); i++)
+    {
+      pcl::PointXYZRGB p;
+      p.x = centers[i].x() - tr[0];
+      p.y = centers[i].y() - tr[1];
+      p.z = centers[i].z() - tr[2];
+      p.r = colors[i].r;
+      p.g = colors[i].g;
+      p.b = colors[i].b;
+      cloud.push_back(p);
+    }
+  }
+  else
+    std::cout << __PRETTY_FUNCTION__ << "nuttingham found " << centers.size() << " " << colors.size() << std::endl;
+  cloud.header.frame_id = _tfMyFrameID;
+  cloud.header.seq      = seq++;
+  std::cout << __PRETTY_FUNCTION__ << " publishing " << cloud.size() << " points" << std::endl;
+}
+
+void PolarSensors::pubSensorRaycast(pcl::PointCloud<pcl::PointXYZ>& cloud)
+{
+  static obvious::obfloat* coords  = new obvious::obfloat[_cellsX * _cellsY * _cellsZ * 3];
+  static obvious::obfloat* normals = new obvious::obfloat[_cellsX * _cellsY * _cellsZ * 3];
+  static unsigned char*    rgb     = new unsigned char[_cellsX * _cellsY * _cellsZ * 3];
+  static unsigned int      seq     = 0;
+
+  unsigned int width  = _sensor->getWidth();
+  unsigned int height = _sensor->getHeight();
+  unsigned int size   = 0;
+
+  obvious::RayCast3D raycasterSensor;
+  raycasterSensor.calcCoordsFromCurrentPose(_space.get(), _sensor.get(), coords, normals, rgb, &size);
+  obvious::obfloat tr[3];
+  _space->getCentroid(tr);
+
+  for(unsigned int i = 0; i < size; i += 3)
+  {
+    pcl::PointXYZ p;
+    p.x = coords[i] - tr[0];
+    p.y = coords[i + 1] - tr[1];
+    p.z = coords[i + 2] - tr[2];
+    cloud.push_back(p);
+  }
+
+  cloud.header.frame_id = _tfMyFrameID;
+  cloud.header.seq      = seq++;
+  std::cout << __PRETTY_FUNCTION__ << " publishing " << cloud.size() << " points" << std::endl;
+}
+
 void PolarSensors::callbackPointcloud(const pcl::PointCloud<pcl::PointXYZ>& cloud)
 {
   // if(!_sensor || !_space) // if neither sensor nor space have been initialized yet -> do so in init routine
   if(!_space) // if space hasn't been initialized yet -> do so in init routine
     this->init(cloud);
+
+  if(_tfActive) // transform sensor
+  {
+    tf::StampedTransform tfSensor;
+    // look up transform from pointcloud frame_id to _tfMyFrameID=map for my sensor to transform sensor!
+    try
+    {
+      _listener->waitForTransform(_tfCallbackFrameID, _tfMyFrameID, ros::Time(0), ros::Duration(10.0));
+      _listener->lookupTransform(_tfCallbackFrameID, _tfMyFrameID, ros::Time(0), tfSensor);
+    }
+    catch(const tf::TransformException& e)
+    {
+      std::cout << __PRETTY_FUNCTION__ << "e: " << e.what() << std::endl;
+      return;
+    }
+    std::cout << __PRETTY_FUNCTION__ << "tfSensorBefore:  translation: " << tfSensor.getOrigin().getX() << " , " << tfSensor.getOrigin().getY() << " , "
+              << tfSensor.getOrigin().getZ() << std::endl;
+
+    tf::Vector3    tfVec = tfSensor.getOrigin();
+    tf::Quaternion quat  = tfSensor.getRotation();
+    tf::Matrix3x3  RotMat(quat);
+    double         roll, pitch, yaw = 0.0;
+    RotMat.getRPY(roll, pitch, yaw);
+
+    // transform sensor
+    obvious::Matrix TransMat(4, 4);
+    TransMat.setIdentity();
+    obvious::obfloat center[3];
+    _space->getCentroid(center);
+    TransMat =
+        obvious::MatrixFactory::TransformationMatrix44(yaw, pitch, roll, center[0] + tfVec.getX(), center[1] + tfVec.getY(), center[2] + tfVec.getZ());
+    _sensor->setTransformation(TransMat);
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    std::cout << "Current Transformation of sensor (listened to tf + transformed): " << std::endl;
+    _sensor->getTransformation().print();
+  }
+
+  if(!_virginPush) // is set to false in constructor. will be set to true after first push
+  {
+    std::vector<double> depthData(cloud.height * cloud.width, 0.0);
+    bool*               mask = new bool[cloud.height * cloud.width];
+
+    std::cout << "Pointcloud height = " << cloud.height << " , width = " << cloud.width << std::endl;
+
+    unsigned int valid = 0;
+
+    for(unsigned int i = 0; i < cloud.width; i++)
+    {
+      for(unsigned int j = 0; j < cloud.height; j++)
+      {
+        const unsigned int idx = i * cloud.height + j;
+
+        Eigen::Vector3f point(cloud.points[idx].x, cloud.points[idx].y, cloud.points[idx].z);
+        double          abs;
+
+        if(_sensorType == "OUSTEROS0")
+        {
+          // call sort function if needed. Ouster tech support said that each measurement carries a ring parameter which specifies the vertical index
+          // check out in which order data is sorted and then push measurement rays in order from inclMin = -45° to inclMax = 45° into vector depthData
+          // in this way, no lookupIndex function is needed
+        }
+        else if(_sensorType == "TILT3D")
+        {
+          // probably not necessary since tilt3d software looks like scans are already collected in the correct order
+          // since tilt3d works with SICK TIM571 which is a 2D LIDAR, there won't be multiple measurements per inclination ray
+          // just delete this statement so that for TILT3D also the "else" condition applies
+          // if sorting is needed: add a method for sorting like in tilt4d
+        }
+        else if(_sensorType == "TILT4D")
+        {
+          // problem here: tilt4d tilts the 16 vertical rays of VLP16 and this results in more measurements per inclination position/ray
+          // implement a sort function that sorts each measurement in a bucket, compute average of each bucket so the result is one single measurement
+          // per inclination ray. The total number of inclination depth measurements must not exceed the number of inclination rays which can be determined
+          // by calulating |inclMin - inclMax| / inclRes
+
+          pcl::PointCloud<pcl::PointXYZ> sortedCloud;
+          this->sortPointcloudTilt4d(&cloud, &sortedCloud);
+          Eigen::Vector3f sortedPoint(sortedCloud.points[idx].x, sortedCloud.points[idx].y, sortedCloud.points[idx].z);
+          abs = static_cast<double>(sortedPoint.norm());
+        }
+        else // fill in depthData in order of scan pointcloud. this is currently done for VLP16 and HDL32E because SensorPolar implements a lookupIndex
+             // function for these two which takes care of the sorting with help of the firing sequence specified in _firingSeq
+        {
+          abs = static_cast<double>(point.norm());
+        }
+
+        if(abs > 0.0)
+        {
+          depthData[idx] = abs;
+          mask[idx]      = true;
+          valid++;
+        }
+        else
+        {
+          mask[idx] = false;
+        }
+      }
+    }
+
+    if(!valid)
+    {
+      std::cout << __PRETTY_FUNCTION__ << " no valid points with depth value > 0.0 in data " << std::endl;
+      return;
+    }
+    std::cout << __PRETTY_FUNCTION__ << " pushing " << valid << " valid points " << std::endl;
+    _sensor->setRealMeasurementData(depthData.data());
+    _sensor->setRealMeasurementMask(mask);
+
+    delete mask;
+    _space->push(_sensor.get());
+    _virginPush = true;
+  }
+  else
+  {
+    // CALL RAYCAST METHODS HERE
+    this->pubAxisAlignedRaycaster();
+
+    pcl::PointCloud<pcl::PointXYZRGB> redBlueRenderedCloud;
+    this->redBlueRenderSpace(redBlueRenderedCloud);
+    _pubRedBlueRendered.publish(redBlueRenderedCloud);
+
+    pcl::PointCloud<pcl::PointXYZ> sensorRaycastCloud;
+    this->pubSensorRaycast(sensorRaycastCloud);
+    _pubSensorRaycastCloud.publish(sensorRaycastCloud);
+  }
 }
--- src/PolarSensors.h
+++ src/PolarSensors.h
@@ -13,6 +13,7 @@
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 
+typedef std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > stdVecEig3d;
 class PolarSensors
 {
 public:
@@ -22,14 +23,24 @@ public:
 
 private:
   void init(const pcl::PointCloud<pcl::PointXYZ>& cloud);
+  void sortPointcloudTilt4d(const pcl::PointCloud<pcl::PointXYZ>* cloud, pcl::PointCloud<pcl::PointXYZ>* sortedCloud);
+  void pubAxisAlignedRaycaster(void);
+  void redBlueRenderSpace(pcl::PointCloud<pcl::PointXYZRGB>& cloud);
+  void pubSensorRaycast(pcl::PointCloud<pcl::PointXYZ>& cloud);
 
   std::unique_ptr<obvious::TsdSpace>    _space;
   std::unique_ptr<obvious::SensorPolar> _sensor;
 
   ros::Subscriber _subPointcloud;
+  ros::Publisher  _pubAxisAlignedCloud;
+  ros::Publisher  _pubRedBlueRendered;
+  ros::Publisher  _pubSensorRaycastCloud;
 
-  ros::NodeHandle _nh;
-  ros::NodeHandle _prvnh;
+  ros::NodeHandle                        _nh;
+  ros::NodeHandle                        _prvnh;
+  std::unique_ptr<tf::TransformListener> _listener;
+  std::string                            _tfMyFrameID;
+  std::string                            _tfCallbackFrameID;
 
   int              _raysIncl;
   std::string      _sensorType;
@@ -40,13 +51,21 @@ private:
   double           _azimMin;
   double           _azimMax;
   double           _azimRes;
-  std::vector<int> _firingSeq;
+  std::vector<int> _firingSeq; // firingSeq order in which vertical lasers are fired (see SensorPolar class Constructor+lookupIndex methods for more
+  // details), is by default empty. If not empty, it will trigger a call to lookupIndex in function backProject() of C SensorPolar
+  // firingSeq for Velodyne VLP16: firingSeq={0, 2, 4, 6, 8, 10, 12, 14, 1, 3, 5, 7, 9, 11, 13, 15}; (check VLP16 User Manual p. 54ff)
+  // note: since the firing sequence (scan order) of the VLP16 does not increment evenly from inclMin to inclMax in inclRes steps, the lookupIndex
+  // method is necessary to match the scan order to the raycast order
+  // firingSeq for Velodyne HDL-32E: firingSeq={0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30,  1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25,
+  // 27, 29, 31};
 
-  float _dimX;
-  float _dimY;
-  float _dimZ;
-  float _cellSize;
-  float _cellsX;
-  float _cellsY;
-  float _cellsZ;
-}
+  float        _dimX;
+  float        _dimY;
+  float        _dimZ;
+  float        _cellSize;
+  unsigned int _cellsX;
+  unsigned int _cellsY;
+  unsigned int _cellsZ;
+  bool         _tfActive;   // deactivate tf frame shifts for debugging reasons
+  bool         _virginPush; // only pushes first pointcloud that arrives in callback and performs raycasts afterwards. rm this for entire SLAM
+};
